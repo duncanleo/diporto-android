ac2f4ce5bc1cfa71f3e11f080a2be00c951bf83c
Duncan Leo <duncanleo97@gmail.com>
2017-07-17 13:03:03 +0800

    show a map

---
+++ app/src/main/java/me/duncanleo/diporto/activity/RoomActivity.kt
@@ -0,0 +1,90 @@
+package me.duncanleo.diporto.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_room.*
+import me.duncanleo.diporto.R
+
+
+class RoomActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mMap: GoogleMap? = null
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room)
+
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        mapView.onCreate(mapViewBundle)
+
+        mapView.getMapAsync(this@RoomActivity)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        var mapViewBundle = outState?.get(MAPVIEW_BUNDLE_KEY) as? Bundle
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState?.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+}
--- app/src/main/java/me/duncanleo/diporto/adapter/RoomsRecyclerViewAdapter.kt
+++ app/src/main/java/me/duncanleo/diporto/adapter/RoomsRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package me.duncanleo.diporto.adapter
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.widget.TextView
 import me.duncanleo.diporto.R
 import me.duncanleo.diporto.model.Room
 import kotlinx.android.synthetic.main.item_room.view.*
+import me.duncanleo.diporto.activity.RoomActivity
 
 /**
  * Created by duncanleo on 17/7/17.
@@ -24,6 +26,9 @@ class RoomsRecyclerViewAdapter : RecyclerView.Adapter<RoomsRecyclerViewAdapter.V
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.itemView?.setOnClickListener {
+            holder.itemView?.context?.startActivity(Intent(holder.itemView.context, RoomActivity::class.java))
+        }
         holder?.name?.text = data[position].name
     }
 
